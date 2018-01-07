cb921fa87a03cb1cfadd64509616039e9e6839f3
alandias01 <alandias01@yahoo.com>
2018-01-06 21:51:29 -0500

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { HeroComponent } from './hero/hero.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 
 import { HeroModule } from './hero/hero.module';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-      BrowserModule,    
+      BrowserModule,
+      RouterModule.forRoot([
+        {path:'welcome', component: WelcomeComponent},
+        { path: '', redirectTo: 'welcome', pathMatch: 'full'}
+        {path:'heroes', component: HeroComponent}
+      ]),
     HeroModule
   ],
   providers: [],
